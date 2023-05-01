0abc8dc9361e98dcdd84f91053514e5aae48b163
Samarjit Bhogal <samar288@students.cs.ubc.ca>
2023-05-01 16:32:45 -0700

    started fixing navbar the way intented working on shirnk

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import "../styles/navBarStyles.css";
 
 export default function NavBar() {
   window.addEventListener("resize", onResize);
   //TODO: onscroll for navbar resize
-  //window.addEventListener("scroll", onScroll)
+  window.addEventListener("scroll", onScroll)
 
   const [logo, setLogo] = useState(true);
   const [nav, setNav] = useState(true);
-  //const [smallNav, setSmallNav] = useState(false);
 
   function onResize() {
     if (window.innerWidth <= 992) {
@@ -23,13 +22,13 @@ export default function NavBar() {
   }
 
   //onscroll for navbar resize
-  // function onScroll() {
-  //   if (window.scrollY > 0) {
-  //     setSmallNav(true);
-  //   } else {
-  //     setSmallNav(false);
-  //   }
-  // }
+  function onScroll() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      document.getElementById("logo").style.width = "100px";
+    } else {
+      document.getElementById("logo").style.width = "150px";
+    }
+  }
 
   useEffect(() => {
     onResize();
@@ -37,7 +36,8 @@ export default function NavBar() {
 
   return (
     <div className="transparent-w fixed">
-      {<nav className="navbar navbar-expand-lg bg-body-tertiary">
+      {
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
             <button
               className="navbar-toggler toggler-nav"
@@ -57,6 +57,7 @@ export default function NavBar() {
                   className="synch-logo-size"
                   src={require("../images/company-logo.jpg")}
                   alt="logo"
+                  id="logo"
                 ></img>
               </Link>
             )}
@@ -65,24 +66,24 @@ export default function NavBar() {
               <div className="collapse navbar-collapse fs-5" id="navbarNav">
                 <ul className="navbar-nav">
                   <li className="nav-item mb-2">
-                    <a className="nav-link" aria-current="page" href="/">
-                      HOME
-                    </a>
+                    <NavLink to="/" className="nav-link">
+                      <p>HOME</p>
+                    </NavLink>
                   </li>
                   <li className="nav-item mb-2">
-                    <a className="nav-link" href="/about">
-                      ABOUT US
-                    </a>
+                    <NavLink to="/about" className="nav-link">
+                      <p>ABOUT US</p>
+                    </NavLink>
                   </li>
                   <li className="nav-item mb-2">
-                    <a className="nav-link" href="/contact">
-                      CONTACT US
-                    </a>
+                    <NavLink to="/contact" className="nav-link">
+                      <p>CONTACT US</p>
+                    </NavLink>
                   </li>
                   <li className="nav-item mb-2">
-                    <a className="nav-link" href="/services">
-                      SERVICES
-                    </a>
+                    <NavLink to="/service" className="nav-link">
+                      <p>SERVICES</p>
+                    </NavLink>
                   </li>
                 </ul>
               </div>
@@ -90,20 +91,22 @@ export default function NavBar() {
 
             {!logo && !nav && (
               <div
-                className="collapse navbar-collapse fs-5 justify-content-center align-items-center"
+                className="collapse navbar-collapse fs-5 justify-content-between"
                 id="navbarNav"
               >
                 <ul className="navbar-nav">
-                  <li className="nav-item p-5">
-                    <a className="nav-link" aria-current="page" href="/">
-                      HOME
-                    </a>
+                  <li className="nav-item m-2">
+                    <NavLink to="/" className="nav-link">
+                      <p>HOME</p>
+                    </NavLink>
                   </li>
-                  <li className="nav-item p-5">
-                    <a className="nav-link" href="/about">
-                      ABOUT US
-                    </a>
+                  <li className="nav-item m-2">
+                    <NavLink to="/about" className="nav-link">
+                      <p>ABOUT US</p>
+                    </NavLink>
                   </li>
+                </ul>
+                <ul className="navbar-nav">
                   <li>
                     <Link to="/">
                       <img
@@ -113,115 +116,24 @@ export default function NavBar() {
                       ></img>
                     </Link>
                   </li>
-                  <li className="nav-item p-5">
-                    <a className="nav-link" href="/contact">
-                      CONTACT US
-                    </a>
+                </ul>
+                <ul className="navbar-nav">
+                  <li className="nav-item m-2">
+                    <NavLink to="/contact" className="nav-link">
+                      <p>CONTACT US</p>
+                    </NavLink>
                   </li>
-                  <li className="nav-itemF p-5">
-                    <a className="nav-link" href="/services">
-                      SERVICES
-                    </a>
+                  <li className="nav-item m-2">
+                    <NavLink to="/service" className="nav-link">
+                      <p>SERVICES</p>
+                    </NavLink>
                   </li>
                 </ul>
               </div>
             )}
           </div>
-        </nav>}
-
-      {/*onscroll for navbar resize:
-       {<nav className="navbar navbar-expand-lg bg-body-tertiary h-nav-lg">
-          <div className="container-fluid">
-            <button
-              className="navbar-toggler toggler-nav"
-              type="button"
-              style={{ border: "none", outline: "none" }}
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-
-            {logo && (
-              <Link to="/">
-                <img
-                  className="synch-logo-size"
-                  src={require("../images/company-logo.jpg")}
-                  alt="logo"
-                ></img>
-              </Link>
-            )}
-
-            {nav && (
-              <div className="collapse navbar-collapse fs-5" id="navbarNav">
-                <ul className="navbar-nav">
-                  <li className="nav-item mb-2">
-                    <a className="nav-link" aria-current="page" href="/">
-                      HOME
-                    </a>
-                  </li>
-                  <li className="nav-item mb-2">
-                    <a className="nav-link" href="/about">
-                      ABOUT US
-                    </a>
-                  </li>
-                  <li className="nav-item mb-2">
-                    <a className="nav-link" href="/contact">
-                      CONTACT US
-                    </a>
-                  </li>
-                  <li className="nav-item mb-2">
-                    <a className="nav-link" href="/services">
-                      SERVICES
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            )}
-
-            {!logo && !nav && (
-              <div
-                className="collapse navbar-collapse fs-5 justify-content-center align-items-center"
-                id="navbarNav"
-              >
-                <ul className="navbar-nav">
-                  <li className="nav-item p-5">
-                    <a className="nav-link" aria-current="page" href="/">
-                      HOME
-                    </a>
-                  </li>
-                  <li className="nav-item p-5">
-                    <a className="nav-link" href="/about">
-                      ABOUT US
-                    </a>
-                  </li>
-                  <li>
-                    <Link to="/">
-                      <img
-                        className="synch-logo-size"
-                        src={require("../images/company-logo.jpg")}
-                        alt="logo"
-                      ></img>
-                    </Link>
-                  </li>
-                  <li className="nav-item p-5">
-                    <a className="nav-link" href="/contact">
-                      CONTACT US
-                    </a>
-                  </li>
-                  <li className="nav-itemF p-5">
-                    <a className="nav-link" href="/services">
-                      SERVICES
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            )}
-          </div>
-        </nav>} */}
+        </nav>
+      }
     </div>
   );
 }
