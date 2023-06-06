f9b99dd3032c63a0122e96c914f0f20a2a48b983
Samarjit Bhogal <samar288@students.cs.ubc.ca>
2023-06-06 13:37:09 -0700

    fixed up contact page and started services

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -43,9 +43,13 @@ export default function Contact() {
         SEND MESSAGE
       </button>
       <h4 className="synch-font">SYNCHRONIZED IMMIGRATION CORP.</h4>
-      <div className="container d-flex justify-content-evenly mt-3 mb-3">
-        <div className="text-center">
-          <h5 className="synch-font">Hours Of Operation</h5>
+      <div className="container d-flex justify-content-evenly">
+        <h5 className="synch-font box">Hours Of Operation</h5>
+        <h5 className="synch-font box">Location</h5>
+        <h5 className="synch-font box">Email and Phone</h5>
+      </div>
+      <div className="container d-flex justify-content-evenly">
+        <div className="text-center box">
           <p className="sm-text">
             <span>
               Mon 09:00 a.m. - 05:00 p.m. <br></br>
@@ -58,12 +62,27 @@ export default function Contact() {
             </span>
           </p>
         </div>
-        <div className="text-center">
-          <h5 className="synch-font">Email and Phone</h5>
-          <a className="sm-text hyper-link" href="mailto: synchronizedimmigration@gmail.com">
-            synchronizedimmigration@gmail.com
-          </a>
+        <div className="text-center box">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d652.4278765363604!2d-122.89282284028536!3d49.14910168900201!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5485d96d9688016b%3A0x7d04a11d1a5a7f62!2s11957%2080%20Ave%2C%20Delta%2C%20BC%20V4C%200E1!5e0!3m2!1sen!2sca!4v1685061493998!5m2!1sen!2sca"
+            width="400"
+            height="300"
+            style={{ border: 0 }}
+            allowfullscreen="true"
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            title="location"
+          ></iframe>
+        </div>
+        <div className="text-center box">
           <p className="sm-text">
+            <a
+              className="sm-text hyper-link"
+              href="mailto: synchronizedimmigration@gmail.com"
+            >
+              synchronizedimmigration@gmail.com
+            </a>{" "}
+            <br></br>
             <span>
               8047-120th Street, <br></br>
               Unit# 140 Delta, B.C., <br></br>
@@ -73,6 +92,106 @@ export default function Contact() {
           </p>
         </div>
       </div>
+
+      {/* FAQ's
+      
+      <div className="container">
+        <div class="accordion" id="accordionExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                Accordion Item #1
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              class="accordion-collapse collapse show"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                <strong>This is the first item's accordion body.</strong> It is
+                shown by default, until the collapse plugin adds the appropriate
+                classes that we use to style each element. These classes control
+                the overall appearance, as well as the showing and hiding via
+                CSS transitions. You can modify any of this with custom CSS or
+                overriding our default variables. It's also worth noting that
+                just about any HTML can go within the{" "}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+              >
+                Accordion Item #2
+              </button>
+            </h2>
+            <div
+              id="collapseTwo"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                <strong>This is the second item's accordion body.</strong> It is
+                hidden by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the{" "}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree"
+              >
+                Accordion Item #3
+              </button>
+            </h2>
+            <div
+              id="collapseThree"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                <strong>This is the third item's accordion body.</strong> It is
+                hidden by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth
+                noting that just about any HTML can go within the{" "}
+                <code>.accordion-body</code>, though the transition does limit
+                overflow.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 }
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,46 +1,91 @@
-import React from "react"
+import React from "react";
 
 export default function Services() {
-    return (
-        <div className="container" style={{ color: "green"}}>
-            <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-</div>
+  return (
+    <div className="">
+      <div className="d-flex justify-content-evenly">
+        <div class="card text-center mb-3" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title">REAL ESTATE</h5>
+            <p class="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a
+              class="btn btn-primary"
+              data-bs-toggle="collapse"
+              href="#realEstate"
+              role="button"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >
+              Link with href
+            </a>
+          </div>
         </div>
-    );
+        <div class="card text-center mb-3" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title">WILLS & POWER OF ATTORNEY'S</h5>
+            <p class="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a
+              class="btn btn-primary"
+              data-bs-toggle="collapse"
+              href="#wills"
+              role="button"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >
+              Link with href
+            </a>
+          </div>
+        </div>
+        <div class="card text-center mb-3" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title">NOTARIZATIONS</h5>
+            <p class="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a
+              class="btn btn-primary"
+              data-bs-toggle="collapse"
+              href="#notarizations"
+              role="button"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >
+              Link with href
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex-column justify-content-center">
+        <div class="collapse" id="realEstate">
+          <div class="card card-body">
+            Some placeholder content for the collapse component. This panel is
+            hidden by default but revealed when the user activates the relevant
+            trigger.
+          </div>
+        </div>
+        <div class="collapse" id="wills">
+          <div class="card card-body">
+            Some placeholder content for the collapse component. This panel is
+            hidden by default but revealed when the user activates the relevant
+            trigger.
+          </div>
+        </div>
+        <div class="collapse" id="notarizations">
+          <div class="card card-body">
+            Some placeholder content for the collapse component. This panel is
+            hidden by default but revealed when the user activates the relevant
+            trigger.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
